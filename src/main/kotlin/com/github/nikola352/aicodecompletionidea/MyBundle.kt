6fbc30b719d1ae4242167894f96b7aaf325b352e
GitHub Action <action@github.com>
2025-04-05 21:18:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/nikola352/aicodecompletionidea/MyBundle.kt
+++ src/main/kotlin/com/github/nikola352/aicodecompletionidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nikola352.aicodecompletionidea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/nikola352/aicodecompletionidea/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/nikola352/aicodecompletionidea/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.nikola352.aicodecompletionidea.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
